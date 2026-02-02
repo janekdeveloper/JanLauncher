fff701074f73e0975cfbd3118e10c87d836b7711
JanDev <103539203+janekdeveloper@users.noreply.github.com>
2026-02-02 19:54:02 +0200

    Delete src directory

--- src/main/core/auth/providers/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Export all authentication providers
- */
-export type { IAuthProvider } from "./AuthProvider";
-export { SanasolAuth } from "./SanasolAuth";
-export { HytaleOfficialAuth } from "./HytaleOfficialAuth";
--- src/renderer/i18n/index.ts
+++
@@ -1,2 +0,0 @@
-export { I18nProvider, useI18n } from "./I18nContext";
-export { translations, type Language } from "./translations";
